dea545f4d9a5936b653c1d1a17cd81528b0146b6
Dan Dart <git@dandart.co.uk>
2018-11-04 20:58:08 +0000

    explanations in public profile ctrl

--- public/js/src/controllers/profilectrl.js
+++ public/js/src/controllers/profilectrl.js
@@ -3,7 +3,12 @@ import '../lib/gravatar';
 import {tabulate} from '../lib/tabulate';
 
 export const profileCtrl = () => {
+    // This could be in a library like getUserInfo(): Promise
+    // - I'd also maybe make jQuery have ajax defaults
+    // if I knew that they were always going to be like this.
     $.getJSON({
+        // I like to have things like this in environments too, so they can be changed
+        // per-dev, per-prod, etc
         url: __POE__.endpoints.userInfo,
         headers: {
             Authorization: `Bearer ${__POE__.accessToken}`
