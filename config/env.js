e4739c8e96c6b4b0dbb52f8baaa9c21ad7c3c619
Dan Dart <git@dandart.co.uk>
2018-11-04 20:58:50 +0000

    explanations in env.js

--- config/env.js
+++ config/env.js
@@ -1,7 +1,11 @@
-
+/**
+ * I like to keep the actual env constants and ways to access them apart.
+ * This way, I only need to specify how I'm talking to the environment once
+ * per constant, and allows the constants to change in one place.
+ */
 export default env => ({
     port: env.PORT || 3000,
-    ip: env.IP || '0.0.0.0',
+    ip: env.IP || '0.0.0.0', // This could well be "::" for all it matters.
     oauth: {
         authorizationURL: env.AUTHORIZATION_URL || '',
         tokenURL: env.TOKEN_URL || '',
